53299748c46e7ce704835981eec9177b0365f539
Bharat Mazire <mazire.bharat@gmail.com>
2018-11-20 18:25:13 +0530

    Create Ladderophilia.c

---
+++ BasicsOfProgramming/BasicsOfIO/Ladderophilia/Ladderophilia.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int i;
+    for(i = 0; i < n; i++)
+    {
+        printf("*   *\n");
+        printf("*   *\n");
+        printf("*****\n");
+    }
+    printf("*   *\n");
+    printf("*   *");
+    
+    return(0);
+}
