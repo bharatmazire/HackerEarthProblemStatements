552af25b9f450f2e3d423a08ceb2f69adb2a58dd
Bharat Mazire <mazire.bharat@gmail.com>
2018-11-22 18:07:44 +0530

    Create AliAndHelpingInnocentPeople.c

---
+++ BasicsOfProgramming/BasicsOfIO/AliAndHelpingInnocentPeople/AliAndHelpingInnocentPeople.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+int main()
+{
+    char str[9];
+    scanf("%s",&str);
+    if( ((str[0]-48) + (str[1]-48)) % 2 != 0)
+    {
+        printf("invalid");
+        return (0);
+    }
+    else if(((str[3]-48) + (str[4]-48)) % 2 != 0)
+    {
+        printf("invalid");
+        return (0);
+    }
+    else if(((str[4]-48) + (str[5]-48)) % 2 != 0)
+    {
+        printf("invalid");
+        return (0);
+    }
+    else if(((str[7]-48) + (str[8]-48)) % 2 != 0)
+    {
+        printf("invalid");
+        return (0);
+    }
+    else if((str[2] == 'A') ||(str[2] == 'E') ||(str[2] == 'I') ||(str[2] == 'O') ||(str[2] == 'U') ||(str[2] == 'Y'))
+    {
+        printf("invalid");
+        return (0);
+    }
+    else
+    {
+        printf("valid");
+    }
+    return (0);
+}
